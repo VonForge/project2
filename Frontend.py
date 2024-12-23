a513879ddaeaad355a29d3778b4bdb89f78c3eaf
Morokyson4ik <miroslavzilaev@gmail.com>
2024-12-23 19:10:12 +0300

    Update Frontend.py
    ...

--- Frontend.py
+++ Frontend.py
@@ -1,30 +1,6 @@
 from flask import Flask
-from flask import render_template
-app = Flask(name)
 
-@app.route('/')
-def hello_world():
-  return 'Hello, World!'
-
-if name == 'main':
-  app.run(debug=True)
-#
-python app.py
-#
-@app.route('/about')
-def about():
-  return 'This is the about page'
-#
-app = Flask(name)
-
-@app.route('/hello/<name>')
-def hello(name):
-  return render_template('hello.html', name=name)
-
-if name == 'main':
-  app.run(debug=True)
-#
-
+app = Flask(__name__)
 
 
 
