5d2a124e16ef6b00aa4e4397eeecfed088774639
Morokyson4ik <miroslavzilaev@gmail.com>
2025-01-16 19:40:05 +0300

    Update HTML.py

--- HTML.py
+++ HTML.py
@@ -2,44 +2,39 @@
 <html lang="ru">
 <head>
   <meta charaset="UTF-8">
-  <title>Сайт системы "SisteMon"
-  </title>
+  <title>"SisteMon"</title>
   <link rel='stylesheet' href='style.css'>
 </head>
 <body>
-  <header class="site-header">
-    <h1>Сайт системы "SisteMon"</h1>
-    <img src="Этот комьютер/Изображения/Plant_project_img_001" alt="" widht='40' height='40'> #если что src изменим
-    <section class='BasicTools'>
-      <h3>секции, как вернутся на начальную страницу,
-        помотреть оповещения</h3>
-    </section>
-  </header>
-    <o>
-      <Pass-Log method='POST'>
-        <div class='conteiner'>
-          <label for='login'> <a> Login </a> </label>
-          <input type='text'>
-          <label for='PassworD'> <a> Password </a> </label>
-          <input type='password' name='Password'>
-          <button type='button' name ='Insert'><svg wight='50' height='30'></svg></button>
-        </div>
-      </Pass-Log>
-      <button type='button' name ='Vive_Monsis'>
-          <svg wight='40' height='20'>
-          </svg>
-        Посмотреть подключённые устройства
-      </button>
-    </o>
-    <p>
-    </p>
-    <select class='List'>
-      <option value='GeneralCondition'>{condition_plant_new}</option>
-      <option value='DeviceNumber'>{Number_device}</option>
-    </select>
-  <footer>
-    <p>Справочная информация
-    </p>
-  </footer>
+  <div class="site-header">
+    <h1>"SisteMon"</h1>
+    <!--
+    <nav>
+        <ul class='BasicTools'>
+            <li><a href="index.html">Главная</a></li>
+            <li><a href="Pass-login.html">один конец</a></li>
+            <li><a href="User_Device.html">Устройства</a></li>
+            <li><a href="Шаблон.html">Шаблон</a></li>
+        </ul>
+    </nav>
+    -->
+  </div>
+  <mn>
+    <div class="main"></div>
+    <div class="block">
+      <div class="container-L">Освещённость </div>
+      <div class="container-VP">Влажность почвы</div>
+      <div class="container-VV">Влажность воздуха</div>
+      <div class="container-TV">Температура воздуха</div>>
+    </div>
+  </mn>
+  <mo>
+    <div class="container-L-rec">Освещённость график</div>
+    <div class="container-VP-rec">Влажность почвы график</div>
+    <div class="container-VV-rec">Влажность воздуха график</div>
+    <div class="container-TV-rec">Температура воздуха график</div>
+  </mo>
 </body>
 </html>
+
+
