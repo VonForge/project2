df7289f4d7db34ba5ade9f5833a5f7c0d3d12eba
Morokyson4ik <miroslavzilaev@gmail.com>
2025-01-26 10:52:26 +0300

    Update index.py

--- index.py
+++ index.py
@@ -3,6 +3,7 @@
 <head>
   <meta charaset="UTF-8">
   <title>"SisteMon"</title>
+  <link rel="icon" href="Картинки/Иконка_сайта.png">
   <link rel='stylesheet' href='style.css'>
 </head>
 <body>
@@ -12,43 +13,66 @@
   <mn>
     <div class="main">
     <div class="block">
-      <div class="container-L">
-        Освещённость(Ligt_Ard)
-      </div>
-      <div class="container-VP">
-        Влажность почвы(SoilHumidity_Ard)
-      </div>
-      <div class="container-VV">
-        Влажность воздуха(AirHumidity_Ard)
-      </div>
-      <div class="container-TV">
-        Температура воздуха(AirTemperature_Ard)
-      </div>
       <div class="container-ind">
         Индекс устройства: 0001-AAA
       </div>
+      <label for="item-select" class="item-select">Растение:</label>
+      <select id="item-select" onchange="TextPlant()">
+          <option value="">Эхинокактус Грузона</option>
+          <option value="NP2">Растение 2</option>
+          <option value="NP3">Растение 3</option>
+      </select>
+      <h2>Выбранное растение</h2>
+      <div id="display-text" class="display-text">Выберите растение</div>
+      <div class="container-L">
+        Освещённость {{ Ligt_Ard }}!
+      </div>
+      <div class="container-VP">
+        Влажность почвы {{SoilHumidity_Ard }}!
+      </div>
+      <div class="container-VV">
+        Влажность воздуха {{AirHumidity_Ard}}!
+      </div>
+      <div class="container-TV">
+        Температура воздуха {{AirTemperature_Ard}}!
+      </div>
       <div class="container-L-rec">
-        Освещённость график(Ligt_Rec)
+        Освещённость график {{Ligt_Rec}}!
       </div>
       <div class="container-VP-rec">
-        Влажность почвы график(SoilHumidity_Rec)
+        Влажность почвы график {{SoilHumidity_Rec}}!
       </div>
       <div class="container-VV-rec">
-        Влажность воздуха график(AirHumidity_Rec)
+        Влажность воздуха график {{AirHumidity_Rec}}!
       </div>
       <div class="container-TV-rec">
-        Температура воздуха график(AirTemperature_Rec)
+        Температура воздуха график {{AirTemperature_Rec}}!
       </div>
       <!-- выше это отображенние данных -->
-       <select class="List-Plant">
-        <option></option>
-        <option></option>
-        <option></option>
-        <option></option>
-       </select>
+      
+       <!-- Список растений -->
     </div>
     </div>
   </mn>
+  <script>
+    function TextPlant() {
+        var selectElement = document.getElementById("item-select");
+        var displayText = document.getElementById("display-text");
+
+        var selectedValue = selectElement.value;
+
+        switch (selectedValue) {
+            case "NP2":
+                displayText.textContent = "Растение 2.";
+                break;
+            case "NP3":
+                displayText.textContent = "Растение 3.";
+                break;
+            default:
+                displayText.textContent = "Эхинокактус Грузона представляет собой небольшой кактус с округлой основой и приплюснутыми ребрами, которые образуют круг. Он достигает в высоту примерно от 5 до 15 см. Стебель у него толстый и плотный, покрытый многочисленными, короткими, острыми иголками. Каждое ребро укрыто острыми шипами, которые могут достигать длины до 5 см. Поскольку этот кактус имеет густую и толстую кору, он хорошо защищен от насекомых и других вредителей.";
+        }
+    }
+</script>
 </body>
 </html>
 
