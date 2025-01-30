646b7ce6b3b8c790ab3a32ee0f8b51fb9d43d7d5
Morokyson4ik <miroslavzilaev@gmail.com>
2025-01-30 16:56:33 +0300

    Update New_Backend.py

--- New_Backend.py
+++ New_Backend.py
@@ -1,17 +1,28 @@
 from flask import Flask
 import sqlite3
+def InSert(Nn, time, AirT, AirH, SoilH, Light):
+  cursor.execute('INSERT INTO BD_Plant (N, Time, AirTemperature_Ard, AirHumidity_Ard, SoilHumidity_Ard, Ligdt_Ard) VALUES (?, ?, ?, ?, ?, ?)', (Nn, time, AirT, AirH, SoilH, Light))
+def SelecT(nameST, n):
+  cursor.execute(f'SELECT {nameST} FROM BD_Plant WHERE age > ?', (n,))
+
 
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
+
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Users (
+CREATE TABLE IF NOT EXISTS BD_Plant (
 id INTEGER PRIMARY KEY,
-username TEXT NOT NULL,
-email TEXT NOT NULL,
-age INTEGER
+N INTEGER NOT NULL,
+Time INTEGER NOT NULL,
+AirTemperature_Ard REAL NOT NULL,
+AirHumidity_Ard REAL,
+SoilHumidity_Ard REAL,
+Ligdt_Ard REAL,
+)
 ''')
-cursor.execute('CREATE INDEX idx_email ON Users (email)')
+
+'''
 cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', ('newuser', 'newuser@example.com', 28))
 # Обновляем возраст пользователя "newuser"
 cursor.execute('UPDATE Users SET age = ? WHERE username = ?', (29, 'newuser'))
@@ -24,13 +35,16 @@ users = cursor.fetchall()
 # Выводим результаты
 for user in users:
   print(user)
+'''
+
+'''
 # Выбираем имена и возраст пользователей старше 25 лет
 cursor.execute('SELECT username, age FROM Users WHERE age > ?', (25,))
 results = cursor.fetchall()
 
 for row in results:
   print(row)
-
+'''
 
 connection.commit()
 connection.close()
