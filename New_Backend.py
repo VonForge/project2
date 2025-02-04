09f2f89f317a37e52caf703356907ca75c35ec0b
Morokyson4ik <miroslavzilaev@gmail.com>
2025-02-04 15:58:22 +0300

    Update New_Backend.py

--- New_Backend.py
+++ New_Backend.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from socket import *
 import sqlite3
 def InSert(Nn, Time, AirT, AirH, SoilH, Light):
   cursor.execute('INSERT INTO BD_Plant (N, Time, AirTemperature_Ard, AirHumidity_Ard, SoilHumidity_Ard, Ligdt_Ard) VALUES (?, ?, ?, ?, ?, ?)', (Nn, Time, AirT, AirH, SoilH, Light))
@@ -30,6 +31,27 @@ Ligdt_Ard REAL NOT NULL,
 )
 ''')
 
-
 connection.commit()
 connection.close()
+
+con = sqlite3.connect('BD_Plant')
+cur = con.cursor()
+
+sock = socket(AF_INET, SOCK_STREAM)
+sock.bind(("", 700))
+
+sock.listen(1)
+
+while True:
+    conn, addr = sock.accept()
+    print('Connected by', addr)
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        print('Received data:', data.decode())
+
+
+
+
+
